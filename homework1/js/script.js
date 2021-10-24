a0b19bc3411e21858cdde52274834c11a299507a
LilIceBear <lillyholub@gmail.com>
2021-10-24 11:15:45 +0300

    made final changes homework1

--- homework1/js/script.js
+++ homework1/js/script.js
@@ -2,22 +2,22 @@
 let userActualAge = 23;
 console.log( 'userActualAge', userActualAge);
 // Ім я користувача
-let userFirstName = "Liliia";
+let userFirstName = 'Liliia';
 console.log( 'userFirstName', userFirstName);
 // Прізвище користувача
-let userLastName = "Stark"
+let userLastName = 'Stark';
 console.log( 'userLastName', userLastName);
-let newUser = 'New user'
+let newUser = 'New user';
 console.log( 'newUser' ,newUser);
 
 //список покупок
-let userGroceryList = "List of user's products";
+let userGroceryList = 'List of users products';
 console.log('userGroceryList', userGroceryList);
 // чи юзер онлайн?
 const isUserOnline = true;
-if (isUserOnline) {console.log ("User is online")} else{console.log ("User is offline")}
+if (isUserOnline) {console.log ('User is online')} else{console.log ('User is offline')}
 const isUserOffline = false;
-if (isUserOnline) {console.log ("User is online")}
+if (isUserOnline) {console.log ('User is online')};
 //зарплата працівнка
 const userAdvance = 10000;
 const userWage =14000;
@@ -33,27 +33,4 @@ const coma =','
 let pointerCoordinates = mousePointerUp + coma+mousePointerDown+ coma+mousePointerLeft + coma+ mousePointerRight;
 console.log('pointerCoordinates', pointerCoordinates);
 
-
-
-const number =12;
-console.log(typeof number, typeof number);
-const decimalNumber = 0.01;
-console.log(decimalNumber);
-const PI = 3.14;
-// const num1 = 1;
-// const num2 = 2;
-// const result = num1+ num2;
-// console.log ('result', result);
-const string = 'Hello';
-console.log ('string', string);
-const decimalNumber1 = 0.5;
-const decimalnumber2 = 0.7;
-const result = decimalNumber1 + decimalnumber2;
-console.log(result);
-
-const greeting = '2';
-const space = ',';
-const name = '3';
-const fullGreeting = greeting + space + name;
-console.log(fullGreeting);
 
